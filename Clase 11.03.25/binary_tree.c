0c09d112a5caec7d43ffd46e79e7e38cf34a5474
PFunes19 <patricio.funessm@gmail.com>
2025-11-03 19:40:55 +0000

    actividad

--- Clase 11.03.25/binary_tree.c
+++ Clase 11.03.25/binary_tree.c
@@ -1,1 +1,60 @@
-g
+#include <stdio.h>
+#include <stdlib.h>
+
+çstruct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+void preorder(struct Node* root) {
+    if (root == NULL)
+        return;
+    printf("%d ", root->data);
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void inorder(struct Node* root) {
+    if (root == NULL)
+        return;
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
+
+void postorder(struct Node* root) {
+    if (root == NULL)
+        return;
+    postorder(root->left);
+    postorder(root->right);
+    printf("%d ", root->data);
+}
+
+int main() {
+    struct Node* root = createNode(10);
+    root->left = createNode(5);
+    root->right = createNode(15);
+    root->left->left = createNode(2);
+    root->left->right = createNode(7);
+
+    printf("Pre-order traversal: ");
+    preorder(root);
+
+    printf("\nIn-order traversal: ");
+    inorder(root);
+
+    printf("\nPost-order traversal: ");
+    postorder(root);
+
+    printf("\n");
+    return 0;
+}
